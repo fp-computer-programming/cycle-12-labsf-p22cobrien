ab5c1e79d95c0cb6fc62707d3d1384cf0c60719d
p22cobrien <p22cobrien@fairfieldprep.org>
2022-02-23 10:08:05 -0500

    submit

---
+++ lab_12-6.py
@@ -0,0 +1,20 @@
+# Auhtor: CMOB 2/23/2022
+
+from shutil import get_archive_formats
+
+
+grades = {'End S1 Labs':100,'Start S2 Labs':100,'cycle 10 labs':0,'Mid-year Project Proposal':100,'cycle 10 practice quiz':100,'cycle 10 quiz':88}
+
+print(grades)
+
+for (k, v) in grades.items():
+    print(k,v)
+
+
+for (k,v) in grades.items():
+    if v > 70:
+        print(k,v)
+
+for (k,v) in grades.items():
+    if v < 50:
+        print(k,v)
